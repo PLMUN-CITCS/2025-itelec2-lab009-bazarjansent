2c459e87b8116e76d0ddf58915535a9f7b9430ed
bazarjansent <178275932+bazarjansent@users.noreply.github.com>
2025-02-14 13:32:11 +0800

    Update if_else_statement.py

--- if_else_statement.py
+++ if_else_statement.py
@@ -4,7 +4,7 @@ if number % 2 == 0:
     print("The number", number, "is Even.")
 else:
     print("The number", number, "is Odd.")
-try:
+    try:
     #code here
 except ValueError:
     print("Invalid input. Please enter an integer.")
